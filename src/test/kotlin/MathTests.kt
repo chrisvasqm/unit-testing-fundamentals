1acdabc567ad8fd2add6a04945fcaf921a4ec34c
Christian Vasquez <christianvasquezmelo@gmail.com>
2022-03-25 20:12:54 -0400

    Add max tests

--- src/test/kotlin/MathTests.kt
+++ src/test/kotlin/MathTests.kt
@@ -1,16 +1,41 @@
 import com.google.common.truth.Truth.assertThat
 import fundamentals.Math
+import org.testng.annotations.BeforeMethod
 import org.testng.annotations.Test
 
 class MathTests {
+    private lateinit var math: Math
+
+    @BeforeMethod
+    fun setUp() {
+        math = Math()
+    }
 
     @Test
     fun add_OnePlusTwo_ReturnsThree() {
-        val math = Math()
-
         val actual = math.add(1, 2)
 
         assertThat(actual).isEqualTo(3)
     }
 
+    @Test
+    fun max_FirstArgumentIsGreater_ReturnFirstArgument() {
+        val actual = math.max(2, 1)
+
+        assertThat(actual).isEqualTo(2)
+    }
+
+    @Test
+    fun max_SecondArgumentIsGreater_ReturnSecondArgument() {
+        val actual = math.max(1, 2)
+
+        assertThat(actual).isEqualTo(2)
+    }
+
+    @Test
+    fun max_BothArgumentsAreEqual_ReturnSameArgument() {
+        val actual = math.max(1, 1)
+
+        assertThat(actual).isEqualTo(1)
+    }
 }
