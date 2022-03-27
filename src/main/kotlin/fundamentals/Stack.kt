23c1cc861550d64441d7fe3721455491754a504e
Christian Vasquez <christianvasquezmelo@gmail.com>
2022-03-27 14:18:04 -0400

    Add Stack

---
+++ src/main/kotlin/fundamentals/Stack.kt
@@ -0,0 +1,35 @@
+package fundamentals
+
+import javax.naming.OperationNotSupportedException
+
+class Stack<T> {
+    private val items = mutableListOf<T>()
+
+    val count: Int
+        get() = items.size
+
+    fun push(item: T) {
+        if (item == null)
+            throw IllegalArgumentException("Item can not be null.")
+
+        items.add(item)
+    }
+
+    fun pop(): T {
+        if (items.size == 0)
+            throw OperationNotSupportedException("The Stack is empty.")
+
+        return items.removeLast()
+    }
+
+    fun peek(): T {
+        if (items.size == 0)
+            throw OperationNotSupportedException("The Stack is empty.")
+
+        return items.last()
+    }
+
+    override fun toString(): String {
+        return "Stack(count=$count, items=$items)"
+    }
+}
