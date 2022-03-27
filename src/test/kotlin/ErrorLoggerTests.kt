6d3cd743bd89ac4a78e6566e3796c8ca5d23e658
Christian Vasquez <christianvasquezmelo@gmail.com>
2022-03-26 20:37:36 -0400

    Add parameterized test

--- src/test/kotlin/ErrorLoggerTests.kt
+++ src/test/kotlin/ErrorLoggerTests.kt
@@ -1,24 +1,42 @@
 import com.google.common.truth.StringSubject
 import com.google.common.truth.Truth.assertThat
 import fundamentals.ErrorLogger
+import org.testng.annotations.BeforeMethod
+import org.testng.annotations.DataProvider
 import org.testng.annotations.Test
 
 class ErrorLoggerTests {
+    private lateinit var logger: ErrorLogger
+
+    @BeforeMethod
+    fun setUp() {
+        logger = ErrorLogger()
+    }
 
     @Test
     fun log_WhenCalled_SavesLastError() {
-        val logger = ErrorLogger()
         logger.log("a")
 
         val actual = logger.lastError
 
         assertThat(actual).isNotNullOrEmpty()
     }
+
+    private fun StringSubject.isNotNullOrEmpty(): Boolean =
+        javaClass.getDeclaredField("actual").let {
+            it.isAccessible = true
+            val value = it.toString()
+            return@let !value.isNullOrEmpty()
+        }
+
+    @Test(expectedExceptions = [IllegalArgumentException::class], dataProvider = "illegal_arguments")
+    fun log_ErrorIsNullOrEmpty_ThrowsIllegalArgumentException(argument: String?) {
+        logger.log(argument)
+    }
+
+    @DataProvider(name = "illegal_arguments")
+    private fun provideIllegalArguments(): MutableIterator<String?> =
+        mutableListOf(null, "", " ").iterator()
+
 }
 
-private fun StringSubject.isNotNullOrEmpty(): Boolean =
-    javaClass.getDeclaredField("actual").let {
-        it.isAccessible = true
-        val value = it.toString()
-        return@let !value.isNullOrEmpty()
-    }
